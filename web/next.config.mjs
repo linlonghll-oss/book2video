ab66062a80acbd86e418f4894ce010db6c766682
linlonghll-oss <linlonghll@gmail.com>
2026-07-29 15:45:03 +0800

    add/update: web/next.config.mjs

---
+++ web/next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8001",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8001",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
